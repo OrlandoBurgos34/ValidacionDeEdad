b4b04f14f60693b1f5cc3b6413092cf5a282ad05
orlando <programador3423@gmail.com>
2023-10-27 14:50:29 -0500

    - new changed to the code

--- validacionDeEdad.swift
+++ validacionDeEdad.swift
@@ -11,3 +11,18 @@ if age < 18{
 }
 
 print("la edad ingresada es \(age)")
+
+//con una funcion quedaria asi
+
+var secondAge: Int = 0
+func stage1AgeVerification() { 
+    print("Qué edad tiene el aspirante: ")
+    secondAge = Int(readLine()!) ?? 0
+    if age < 16 || age > 100 {
+        print("La edad ingresada no es válida")
+        return stage1AgeVerification()
+    } else {
+        print("Excelente, continuemos")
+    }
+}
+stage1AgeVerification()
