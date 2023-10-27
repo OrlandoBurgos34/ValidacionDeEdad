36c2f4d987a474e0f3a24f0967392d09728805d6
orlando <programador3423@gmail.com>
2023-10-27 14:04:46 -0500

    - initial commit

---
+++ validacionDeEdad.swift
@@ -0,0 +1,10 @@
+
+let input = readLine(),let age = Int(input)
+
+if age < 18{
+    print("la edad es incorrecta")
+}else if age > 99 {
+    print("la edad es invalida digite otra edad")
+}else {
+    print("la edad es correcta")
+}
