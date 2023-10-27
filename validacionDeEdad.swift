329d8c0904b249bae02090c23d4cd77907afb22c
orlando <programador3423@gmail.com>
2023-10-27 14:58:02 -0500

    - remove initial code

--- validacionDeEdad.swift
+++ validacionDeEdad.swift
@@ -1,23 +1,11 @@
 
-// validando mayoria de edad
-let age = 34
-
-if age < 18{
-    print("la edad es incorrecta")
-}else if age > 99 {
-    print("la edad es invalida digite otra edad")
-}else {
-    print("la edad es correcta")
-}
-
-print("la edad ingresada es \(age)")
 
 //con una funcion quedaria asi
 
-var secondAge: Int = 0
+var age: Int = 0
 func stage1AgeVerification() { 
     print("Qué edad tiene el aspirante: ")
-    secondAge = Int(readLine()!) ?? 0
+    age = Int(readLine()!) ?? 0
     if age < 16 || age > 100 {
         print("La edad ingresada no es válida")
         return stage1AgeVerification()
